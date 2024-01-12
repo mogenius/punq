b6bc58abb92337ac86b4ef5182e6ebad8769305e
Benedikt Iltisberger <bene@mogenius.com>
2024-01-12 15:47:41 +0100

    fix: minor fix.

--- structs/command.go
+++ structs/command.go
@@ -10,20 +10,20 @@ import (
 )
 
 type Command struct {
-	Id                      string  `json:"id"`
-	JobId                   string  `json:"jobId"`
-	ProjectId               string  `json:"projectId"`
-	NamespaceId             *string `json:"namespaceId,omitempty"`
-	ServiceId               *string `json:"serviceId,omitempty"`
-	Title                   string  `json:"title"`
-	Message                 string  `json:"message,omitempty"`
-	StartedAt               string  `json:"startedAt"`
-	State                   string  `json:"state"`
-	DurationMs              int64   `json:"durationMs"`
-	MustSucceed             bool    `json:"mustSucceed"`
-	ReportToNotificationSvc bool    `json:"reportToNotificationService"`
-	IgnoreError             bool    `json:"ignoreError"`
-	BuildId                 int     `json:"buildId,omitempty"`
+	Id                      string       `json:"id"`
+	JobId                   string       `json:"jobId"`
+	ProjectId               string       `json:"projectId"`
+	NamespaceId             *string      `json:"namespaceId,omitempty"`
+	ServiceId               *string      `json:"serviceId,omitempty"`
+	Title                   string       `json:"title"`
+	Message                 string       `json:"message,omitempty"`
+	StartedAt               string       `json:"startedAt"`
+	State                   JobStateEnum `json:"state"`
+	DurationMs              int64        `json:"durationMs"`
+	MustSucceed             bool         `json:"mustSucceed"`
+	ReportToNotificationSvc bool         `json:"reportToNotificationService"`
+	IgnoreError             bool         `json:"ignoreError"`
+	BuildId                 int          `json:"buildId,omitempty"`
 	Started                 time.Time
 }
 
